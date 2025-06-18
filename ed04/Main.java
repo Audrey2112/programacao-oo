aabb9092d9babeef298017445012d030bc9dfe76
Audrey Cristhiny <audreykochinski@gmail.com>
2025-06-18 09:32:09 -0300

    Create Main.java

---
+++ ed04/Main.java
@@ -0,0 +1,11 @@
+package ed04;
+
+import ed04.controller.ContatoController;
+import ed04.controller.ContatoSwingController;
+
+public class Main {
+    public static void main(String[] args) {
+        ContatoSwingController controller = new ContatoSwingController();
+        controller.executar();
+    }
+}
